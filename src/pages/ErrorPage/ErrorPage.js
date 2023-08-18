39054da381737ca3b704bfeaff68007d29204304
dafanch <lel.francois@gmail.com>
2023-08-18 11:21:47 +0200

    404 page desktop css done

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {
+  Link as ReactRouterLink,
+} from 'react-router-dom';
 
 const ErrorPage = () => (
-  <h1>Page 404</h1>
+  <>
+    <h1 className="h1-404">404</h1>
+    <h2 className="h2-404">Oups! La page que vous demandez n'existe pas.</h2>
+    <div className="text-align-center margin-bottom-100px">
+      <ReactRouterLink to="/" className="link-404">
+        Retourner sur la page d'accueil
+      </ReactRouterLink>
+    </div>
+  </>
 );
 
 export default ErrorPage;
